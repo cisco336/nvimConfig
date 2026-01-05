faacca8917d1a1fb7b33c32c84cfa8ccd8d44595
Francisco José Arleo Amaya <cisco336@gmail.com>
2026-01-05 12:28:07 -0500

    Refactor wezterm configuration for readability

--- .wezterm.lua
+++ .wezterm.lua
@@ -21,20 +21,22 @@ config.font = wezterm.font("JetBrains Mono")
 config.color_scheme = theme
 
 -- This should be after setting color_scheme
-bar.apply_to_config(config, {
-	padding = {
-		left = 2,
-		right = 3,
-	},
-	modules = {
-		username = {
-			enabled = false,
-		},
-		hostname = {
-			enabled = false,
-		},
-	},
-})
+bar.apply_to_config(
+    config, {
+        padding = {
+            left = 2,
+            right = 3
+        },
+        modules = {
+            username = {
+                enabled = false
+            },
+            hostname = {
+                enabled = false
+            }
+        }
+    }
+)
 
 -- Window
 config.window_close_confirmation = "NeverPrompt"
@@ -45,85 +47,105 @@ config.tab_bar_at_bottom = true
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 20
 config.window_padding = {
-	left = "2cell",
-	right = "2cell",
-	top = "1cell",
-	bottom = "1cell",
+    left = "2cell",
+    right = "2cell",
+    top = "1cell",
+    bottom = "1cell"
 }
 config.default_cursor_style = "BlinkingBlock"
 config.cursor_blink_rate = 500
 
 -- Inactive Panes
 config.inactive_pane_hsb = {
-	saturation = 0.6,
-	brightness = 0.5,
+    saturation = 0.6,
+    brightness = 0.5
 }
 
 -- Keys
 config.keys = {
-	{
-		key = "d",
-		mods = "CMD",
-		action = wezterm.action.SplitVertical({
-			domain = "CurrentPaneDomain",
-		}),
-	},
-	{
-		key = "d",
-		mods = "SHIFT|CMD",
-		action = wezterm.action.SplitHorizontal({
-			domain = "CurrentPaneDomain",
-		}),
-	},
-	{
-		key = "w",
-		mods = "CMD",
-		action = wezterm.action.CloseCurrentPane({
-			confirm = false,
-		}),
-	},
-	{
-		key = "w",
-		mods = "SHIFT|CMD",
-		action = wezterm.action.CloseCurrentTab({
-			confirm = false,
-		}),
-	},
-	{
-		key = "o",
-		mods = "CTRL",
-		action = wezterm.action({
-			PaneSelect = {},
-		}),
-	}, -- Show the selector, using your own alphabet
-	{
-		key = "p",
-		mods = "CTRL",
-		action = wezterm.action({
-			PaneSelect = {
-				alphabet = "0123456789",
-			},
-		}),
-	},
-	{
-		key = "RightArrow",
-		mods = "OPT",
-		action = wezterm.action({
-			SendString = "\x1bf",
-		}),
-	},
-	{
-		key = "LeftArrow",
-		mods = "OPT",
-		action = wezterm.action({
-			SendString = "\x1bb",
-		}),
-	},
-	{
-		key = "k",
-		mods = "CMD",
-		action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
-	},
-}
+    {
+        key = "d",
+        mods = "CMD",
+        action = wezterm.action.SplitVertical(
+            {
+                domain = "CurrentPaneDomain"
+            }
+        )
+    }, {
+        key = "d",
+        mods = "SHIFT|CMD",
+        action = wezterm.action.SplitHorizontal(
+            {
+                domain = "CurrentPaneDomain"
+            }
+        )
+    }, {
+        key = "w",
+        mods = "CMD",
+        action = wezterm.action.CloseCurrentPane(
+            {
+                confirm = false
+            }
+        )
+    }, {
+        key = "w",
+        mods = "SHIFT|CMD",
+        action = wezterm.action.CloseCurrentTab(
+            {
+                confirm = false
+            }
+        )
+    }, {
+        key = "o",
+        mods = "CTRL",
+        action = wezterm.action(
+            {
+                PaneSelect = {}
+            }
+        )
+    }, -- Show the selector, using your own alphabet
+    {
+        key = "p",
+        mods = "CTRL",
+        action = wezterm.action(
+            {
+                PaneSelect = {
+                    alphabet = "0123456789"
+                }
+            }
+        )
+    }, {
+        key = "RightArrow",
+        mods = "CTRL",
+        action = wezterm.action.AdjustPaneSize {'Right', 5}
+    }, {
+        key = "LeftArrow",
+        mods = "CTRL",
+        action = wezterm.action.AdjustPaneSize {'Left', 5}
+    }, {
+        key = "UpArrow",
+        mods = "CTRL",
+        action = wezterm.action.AdjustPaneSize {'Up', 5}
+    }, {
+        key = "DownArrow",
+        mods = "CTRL",
+        action = wezterm.action.AdjustPaneSize {'Down', 5}
+    }, {
+        key = "k",
+        mods = "CMD",
+        action = wezterm.action.ClearScrollback("ScrollbackAndViewport")
+    }, {
+        key = "F9",
+        mods = "ALT",
+        action = wezterm.action.ShowTabNavigator
+    }, {
+        key = 'b',
+        mods = 'CTRL',
+        action = wezterm.action.RotatePanes 'CounterClockwise'
+    }, {
+        key = 'n',
+        mods = 'CTRL',
+        action = wezterm.action.RotatePanes 'Clockwise'
+    }}
 
 return config
